bc1a9c4c4c3bc1886b3328b36b3db372d70a17a1
jainsuhani09 <suhanijainmahi09@gmail.com>
2023-12-30 22:04:05 +0530

    fifth commit

--- js/contest.js
+++ js/contest.js
@@ -1,186 +1,389 @@
-let timeLeft = document.querySelector(".time-left");
-let quizContainer = document.getElementById("container");
-let nextBtn = document.getElementById("next-button");
-let countOfQuestion = document.querySelector(".number-of-question");
-let displayContainer = document.getElementById("display-container");
-let scoreContainer = document.querySelector(".score-container");
-let restart = document.getElementById("restart");
-let userScore = document.getElementById("user-score");
-let startScreen = document.querySelector(".start-screen");
-let startButton = document.getElementById("start-button");
-let questionCount;
-let scoreCount=0;
-let count=11;
-let countdown;
-const quizArray = [
+const questions = [
     {
-        id: "0",
         question: "Which of the following is a programming language?",
-        options: [
-           " Python",
-            "Photoshop", 
-            "Microsoft Word",
-            "Adobe Illustrator"
-                    
-        ],
-        correct: "Python",
-    },
-    {
-        id: "1",
-        question: "Which programming paradigm emphasizes on writing code that is easy to read and maintain?",
-        options: [
-            "Procedural programming",
-            "Object-oriented programming",
-            "Functional programming",
-            "Structured programming"
- 
-        ],
-        correct: "Object-oriented programming",
+        optionA:  " Python",
+        optionB:  "Photoshop",
+        optionC: "Microsoft Word",
+        optionD: "Adobe Illustrator",
+        correctOption: "optionA"
     },
 
     {
-        id: "2",
+        question: "Which programming paradigm emphasizes on writing code that is easy to read and maintain?",
+        optionA: "Procedural programming",
+        optionB: "Object-oriented programming",
+        optionC:  "Functional programming",
+        optionD:  "Structured programming",
+        correctOption: "optionB"
+    },
+
+    {
         question: "What is the term used for a block of code that is executed repeatedly until a certain condition is met?",
-        options: [
-            " Function",
-            "Loop",
-            "Condition",
-            "Variable"
- 
-        ],
-        correct: "Loop",
+        optionA: "Function",
+        optionB: "Loop",
+        optionC: "Condition",
+        optionD: "Variable",
+        correctOption: "optionB"
     },
+
     {
-        id: "3",
         question: "Which data structure is used for storing a collection of elements in a non-linear fashion?",
-        options: [
-            "Array",
-            "Stack",
-            "Queue",
-            "Tree"
- 
-        ],
-        correct: " Tree",
+        optionA: "Array",
+        optionB: "Stack",
+        optionC: "Queue",
+        optionD: "Tree",
+        correctOption: "optionD"
     },
+
     {
-        id: "4",
         question: "Which operator is used for exponentiation in most programming languages?",
-        options: [
-            "^",
-            "*",
-            "%",
-            "**"
- 
-        ],
-        correct: "**",
+        optionA: "^",
+        optionB: "*",
+        optionC: "%",
+        optionD:  "**",
+        correctOption: "optionD"
     },
+
     {
-        id: "5",
         question: "Which programming language is used for developing Android apps?",
-        options: [
-            "Java",
-            "C#",
-            "Python",
-            "Swift"
- 
-        ],
-        correct: "Java",
+        optionA: "Java",
+        optionB: "C#",
+        optionC: "Swift",
+        optionD:  "Python",
+        correctOption: "optionA"
     },
+
     {
-        id: "6",
-        question: "Which of the following is not a valid variable name in most programming languages?",
-        options: [
-            "myVariable",
-            "123Variable",
-            "_myVariable",
-            "my_variable"
- 
-        ],
-        correct: "123Variable",
+        question: "_____ is the hottest Continent on Earth ?",
+        optionA: "Oceania",
+        optionB: "Antarctica",
+        optionC: "Africa",
+        optionD: "North America",
+        correctOption: "optionC"
     },
+
     {
-        id: "7",
-        question: " Which of the following is a type of sorting algorithm?",
-        options: [
-            "Binary search",
-            " Quick sort",
-            "Depth-first search",
-            "Breadth-first search"
- 
-        ],
-        correct: "Quick sort",
+        question: "Which country is the largest in the world ?",
+        optionA: "Russia",
+        optionB: "Canada",
+        optionC: "Africa",
+        optionD: "Egypt",
+        correctOption: "optionA"
     },
+
     {
-        id: "8",
-        question: "Which of the following is not a web browser?",
-        options: [
-            "Chrome",
-            "Safari",
-            "Firefox",
-            "Photoshop"
- 
-        ],
-        correct: "Photoshop",
+        question: "Which of these numbers is an odd number ?",
+        optionA: "Ten",
+        optionB: "Twelve",
+        optionC: "Eight",
+        optionD: "Eleven",
+        correctOption: "optionD"
     },
+
     {
-        id: "9",
-        question: "Which keyword is used for defining a function in most programming languages?",
-        options: [
-            "def",
-            "fun",
-            "sub",
-            "function"
- 
-        ],
-        correct: "def",
+        question: `"You Can't see me" is a popular saying by`,
+        optionA: "Eminem",
+        optionB: "Bill Gates",
+        optionC: "Chris Brown",
+        optionD: "John Cena",
+        correctOption: "optionD"
     },
-];
-restart.addEventListener("click",() =>{
-    initial();
-    displayContainer.classList.remove("hide");
-    scoreContainer.classList.add("hide");
-});
-nextBtn.addEventListener("click",(displayNext = ()=>{
-    questionCount += 1;
-    if (questionCount==quizArray.length) {
-        displayContainer.classList.add("hide");
-        scoreContainer.classList.remove("hide");
-        userScore.innerHTML = "Your Score is "+ scoreCount+"  out of "+questionCount;
+
+    {
+        question: "Where is the world tallest building located ?",
+        optionA: "Africa",
+        optionB: "California",
+        optionC: "Dubai",
+        optionD: "Italy",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "The longest river in the United Kingdom is ?",
+        optionA: "River Severn",
+        optionB: "River Mersey",
+        optionC: "River Trent",
+        optionD: "River Tweed",
+        correctOption: "optionA"
+    },
+
+
+    {
+        question: "How many permanent teeth does a dog have ?",
+        optionA: "38",
+        optionB: "42",
+        optionC: "40",
+        optionD: "36",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Which national team won the football World cup in 2018 ?",
+        optionA: "England",
+        optionB: "Brazil",
+        optionC: "Germany",
+        optionD: "France",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which US state was Donald Trump Born ?",
+        optionA: "New York",
+        optionB: "California",
+        optionC: "New Jersey",
+        optionD: "Los Angeles",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "How man states does Nigeria have ?",
+        optionA: "24",
+        optionB: "30",
+        optionC: "36",
+        optionD: "37",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "____ is the capital of Nigeria ?",
+        optionA: "Abuja",
+        optionB: "Lagos",
+        optionC: "Calabar",
+        optionD: "Kano",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Los Angeles is also known as ?",
+        optionA: "Angels City",
+        optionB: "Shining city",
+        optionC: "City of Angels",
+        optionD: "Lost Angels",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "What is the capital of Germany ?",
+        optionA: "Georgia",
+        optionB: "Missouri",
+        optionC: "Oklahoma",
+        optionD: "Berlin",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "How many sides does an hexagon have ?",
+        optionA: "Six",
+        optionB: "Sevene",
+        optionC: "Four",
+        optionD: "Five",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "How many planets are currently in the solar system ?",
+        optionA: "Eleven",
+        optionB: "Seven",
+        optionC: "Nine",
+        optionD: "Eight",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which Planet is the hottest ?",
+        optionA: "Jupitar",
+        optionB: "Mercury",
+        optionC: "Earth",
+        optionD: "Venus",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "where is the smallest bone in human body located?",
+        optionA: "Toes",
+        optionB: "Ears",
+        optionC: "Fingers",
+        optionD: "Nose",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "How many hearts does an Octopus have ?",
+        optionA: "One",
+        optionB: "Two",
+        optionC: "Three",
+        optionD: "Four",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "How many teeth does an adult human have ?",
+        optionA: "28",
+        optionB: "30",
+        optionC: "32",
+        optionD: "36",
+        correctOption: "optionC"
     }
-    else{
-        countOfQuestion.innerHTML = questionCount+1+
-        " of "+ quizArray.length + " Question ";
-        quizDisplay(questionCount);
-        count=11;
-        clearInterval(countdown);
-        timerDisplay();
-    }
-}));
-const timerDisplay = () => {
-    countdown =  setInterval(() => {
-        count--;
-        timeLeft.innerHTML = `${count}s`;
-        if (count==0){
-            clearInterval(countdown);
-            displayNext();
+
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
+
+function handleQuestions() { 
+    //function to shuffle and push 10 questions to shuffledQuestions array
+//app would be dealing with 10questions per session
+    while (shuffledQuestions.length <= 9) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
         }
-    }, 1000);
-};
-const quizDisplay = (questionCount) => {
-    let quizCards = document.querySelectorAll(".container-mid");
-    quizCards.forEach((card) => {
-        card.classList.add("hide");
-    });
-    quizCards[questionCount].classList.remove("hide");
-};
-function quizCreater(){
-    quizArray.sort(()=> Math.random() -0.5);
-    for(let i of quizArray){
-        i.options.sort(()=> Math.random() -0.5);
-        let div = document.createElement("div");
-        div.classList.add("container-mid","hide");
-        countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question "
     }
-    let question_Div = document.createElement("p");
-    question_Div.classList
+}
+
+
+let questionNumber = 1 //holds the current question number
+let playerScore = 0  //holds the player score
+let wrongAttempt = 0 //amount of wrong answers picked by player
+let indexNumber = 0 //will be used in displaying next question
+
+// function for displaying next question in the array to dom
+//also handles displaying players and quiz information to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++ //adding to player's score
+            indexNumber++ //adding 1 to index so has to display next question..
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++ //adds 1 to wrong attempts 
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer() //check if player picked right or wrong option
+    unCheckRadioButtons()
+    //delays next question displaying for a second just for some effects so questions don't rush in on player
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    if (playerScore <= 3) {
+        remark = "Bad Grades, Keep Practicing."
+        remarkColor = "red"
+    }
+    else if (playerScore >= 4 && playerScore < 7) {
+        remark = "Average Grades, You can do better."
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 7) {
+        remark = "Excellent, Keep the good work going."
+        remarkColor = "green"
+    }
+    const playerGrade = (playerScore / 10) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal, resets game and reshuffles questions
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
 }
